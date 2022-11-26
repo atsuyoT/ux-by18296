39e06c2d65ac35b28a8b1023ff9456c07f384970
atsuyoT <100555063+atsuyoT@users.noreply.github.com>
2022-11-27 01:46:40 +1100

    Add files via upload

---
+++ JS/example.js
@@ -0,0 +1,8 @@
+﻿console.log("JavaScript has been loaded...");
+
+$(document).ready(function () {
+    $("#flip").click(function () {
+        $("#panel").slideDown("slow");
+    });
+});
+
---
+++ JS/loading.js
@@ -0,0 +1,23 @@
+﻿console.log('my loading script loading...')
+$(document).ready(function () {
+    var div_box = "<div id='load-screen'><div id='loading'></div></div>";
+    $("body").prepend(div_box);
+    $("#load-screen")
+        .delay(800)
+        .fadeOut(500, function () {
+            $(this).remove();
+        });
+    console.log('loading image has been done...');
+});
+
+$(function () {
+    // this will get the full URL at the address bar
+    var url = window.location.href;
+    // passes on every "a" tag 
+    $(".nav navbar-nav a").each(function () {
+        // checks if its the same on the address bar
+        if (url == (this.href)) {
+            $(this).closest("li").addClass("activebg");
+        }
+    });
+});
